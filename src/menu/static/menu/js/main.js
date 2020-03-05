88d158da97fdcada22a0fcc29512e28c738b3dae
MDsona <MD-S2013@hotmail.com>
2020-03-05 17:37:25 +0300

    basic menu design

---
+++ src/menu/static/menu/js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+    'use strict';
+
+    $('.menu_ti').click(function () {
+
+        $('.menu_ty').slideToggle()
+    });
+
+    $('.menu_ty').click(function () {
+
+        $('.menu_co').slideToggle()
+    });
+
+});
