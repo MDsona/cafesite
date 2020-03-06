432d951da62d64799e8cc70ef02dbe9ab307d735
MDsona <MD-S2013@hotmail.com>
2020-03-06 21:57:46 +0300

    menu type model

--- src/menu/admin.py
+++ src/menu/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import MenuTitle               # 2b
+from .models import MenuTitle, MenuType               # 2b, 6b
 
 # Register your models here.
 
 admin.site.register(MenuTitle)              # 2b
+admin.site.register(MenuType)              # 6b
--- src/menu/models.py
+++ src/menu/models.py
@@ -7,3 +7,11 @@ class MenuTitle(models.Model):                  # 2a
 
     def __str__(self):                          # 2c
         return self.title
+
+
+class MenuType(models.Model):                   # 6a
+    the_type = models.CharField(max_length=100)
+    title = models.ForeignKey(MenuTitle, on_delete=models.CASCADE, related_name='title_type_releted')
+
+    def __str__(self):                          # 6c
+        return self.the_type
