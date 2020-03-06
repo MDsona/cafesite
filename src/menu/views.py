9d50f734cb990188086e59c4bd28096bb916c720
MDsona <MD-S2013@hotmail.com>
2020-03-06 17:25:33 +0300

    to add postgre sql

--- src/menu/views.py
+++ src/menu/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
-from .models import MenuTitle                           # 3a
+# 4a from .models import MenuTitle                           # 3a
 
 # Create your views here.
 
 def home(request):                                      # 1b
-    menu_titles = MenuTitle.objects.all()               # 3a
+    # 4a menu_titles = MenuTitle.objects.all()               # 3a
 
     context = {
         'page_title': 'الرئيسية',                      # 1b>1e
-        'menu_titles': menu_titles,                     # 3a
+        # 4a 'menu_titles': menu_titles,                     # 3a
     }
 
     return render(request, 'menu/index.html', context)
