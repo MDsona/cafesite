7322b592aa7688834721bf90efd1a26a01e7b78b
MDsona <MD-S2013@hotmail.com>
2020-03-06 17:12:14 +0300

    view menu from db.sqlite3

--- src/menu/views.py
+++ src/menu/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
+from .models import MenuTitle                           # 3a
+
 # Create your views here.
 
 def home(request):                                      # 1b
+    menu_titles = MenuTitle.objects.all()               # 3a
 
     context = {
-        'page_title': 'الرئيسية'                       # 1b>1e
+        'page_title': 'الرئيسية',                      # 1b>1e
+        'menu_titles': menu_titles,                     # 3a
     }
 
     return render(request, 'menu/index.html', context)
