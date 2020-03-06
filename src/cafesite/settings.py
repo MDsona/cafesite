fbf8e322c1f825675db6373a34d64eed6f335b60
MDsona <MD-S2013@hotmail.com>
2020-03-06 21:27:49 +0300

    change database to postgres

--- src/cafesite/settings.py
+++ src/cafesite/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'cafesite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',     # 5a
+        'NAME': 'mdcafe',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'Host': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- src/menu/views.py
+++ src/menu/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
-# 4a from .models import MenuTitle                           # 3a
+from .models import MenuTitle                           # 3a
 
 # Create your views here.
 
 def home(request):                                      # 1b
-    # 4a menu_titles = MenuTitle.objects.all()               # 3a
+    menu_titles = MenuTitle.objects.all()               # 3a
 
     context = {
         'page_title': 'الرئيسية',                      # 1b>1e
-        # 4a 'menu_titles': menu_titles,                     # 3a
+        'menu_titles': menu_titles,                     # 3a
     }
 
     return render(request, 'menu/index.html', context)
