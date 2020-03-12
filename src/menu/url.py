ad8420a23fdc0cf7372d0ec21429bef933c8b027
MDsona <MD-S2013@hotmail.com>
2020-03-12 22:14:09 +0300

    open menu

--- src/menu/url.py
+++ src/menu/url.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [                                        
     path('', views.home, name= 'home_url'),            # 1c
     path('menu/<int:titleID>/', views.menu_type, name= 'm_type_url'),  # 7b
+    path('menu', views.open_menu, name= 'open_menu_url'),            # 8b
 ]
--- src/menu/views.py
+++ src/menu/views.py
@@ -5,11 +5,11 @@ from .models import MenuTitle, MenuType                           # 3a
 # Create your views here.
 
 def home(request):                                      # 1b, 
-    menu_titles = MenuTitle.objects.all()               # 3a
+    # 8a menu_titles = MenuTitle.objects.all()               # 3a
 
     context = {
         'page_title': 'الرئيسية',                      # 1b>1e
-        'menu_titles': menu_titles,                     # 3a
+        # 8a 'menu_titles': menu_titles,                     # 3a
     }
 
     return render(request, 'menu/index.html', context)
@@ -28,3 +28,15 @@ def menu_type(request, titleID):                       # 7a
     }
 
     return render(request, 'menu/menu_type.html', context)
+
+
+def open_menu(request):                                     # 8a
+    menu_titles = MenuTitle.objects.all()
+
+    context = {
+        'page_title': 'القائمة',
+        'menu_titles': menu_titles,
+    }
+
+    return render(request, 'menu/open_menu.html', context)
+
