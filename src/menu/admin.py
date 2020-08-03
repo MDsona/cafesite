1df62434944095debd9ce36aeb49a415edc518ec
MDsona <MD-S2013@hotmail.com>
2020-08-03 15:04:21 +0300

    for reservations model

--- src/menu/admin.py
+++ src/menu/admin.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 
-from .models import MenuTitle, MenuType, MenuContent, MapBox               # 2b, 6b, 9b, 12b
+from .models import MenuTitle, MenuType, MenuContent, MapBox, SessionType, NumberOFseats, ForReser   # 2b, 6b, 9b, 12b, 18a,b, 19b
 
 # Register your models here.
 
 admin.site.register(MenuTitle)              # 2b
-admin.site.register(MenuType)              # 6b
+admin.site.register(MenuType)               # 6b
 admin.site.register(MenuContent)            # 9b
 admin.site.register(MapBox)                 # 12b 
+admin.site.register(SessionType)            # 18a
+admin.site.register(NumberOFseats)          # 18b
+admin.site.register(ForReser)               # 19b
 
 
--- src/menu/models.py
+++ src/menu/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.contrib.auth.models import User     #19d
+from django.utils import timezone
+
 # Create your models here.
 
 class MenuTitle(models.Model):                  # 2a
@@ -34,6 +37,33 @@ class MapBox(models.Model):                                 # 12a
 
     def __str__(self):                                      # 12a
         return self.mapURL
+
+
+class SessionType(models.Model):                            # 18a
+    session_type1 = models.CharField(max_length=30, blank=True, verbose_name='نوع الجلسة')
+    def __str__(self):
+        return self.session_type1
+
+class NumberOFseats(models.Model):                          # 18b
+    number_of_seats1 = models.CharField(max_length=10, blank=True, verbose_name='عدد المقاعد', help_text='مثل: 10 مقاعد')
+    def __str__(self):
+        return self.number_of_seats1
+
+
+class ForReser(models.Model):                               # 19a>>
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name='الاسم')  # d19d
+    session_type = models.ForeignKey(SessionType, on_delete=models.CASCADE, blank=True, null=True, verbose_name='نوع الجلسة')  # a19a
+    number_of_seats = models.ForeignKey(NumberOFseats, on_delete=models.CASCADE, blank=True, null=True, verbose_name='عدد المقاعد')
+    session_duration = models.FloatField(help_text='ساعة', blank=True, null=True, verbose_name='مدة الجلسة')      # c19c
+    session_date = models.DateTimeField(null=True, blank=True, verbose_name='التوقيت')
+    mobile_number = models.CharField(max_length=10, help_text='مثال:0511111111', blank=True, null=True, verbose_name='رقم الجوال')
+    timestamp = models.DateTimeField(auto_now=True)             # 19d
+
+    def __str__(self):                                          # 19e
+        return 'حجز {} في تاريخ {}'.format(self.author, self.session_date)
+    
+
+
 
 
 
